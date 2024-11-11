4381fefa8a41837bee10b32d284bf7d85bf17275
ramya23000505 <149370791+ramya23000505@users.noreply.github.com>
2024-11-11 15:49:37 +0530

    Update planning.py

--- planning.py
+++ planning.py
@@ -1,6 +1,35 @@
 # Developed by : RAMYA R
 # Register NUmber: 212223230169
 
+# Example - 1
+```
+initial_state = {'A': 'Table', 'B': 'Table'}
+goal_state = {'A': 'B', 'B': 'Table'}
+
+actions = {
+    'move_A_to_B': {'precondition': {'A': 'Table', 'B': 'Table'}, 'effect': {'A': 'B'}},
+    'move_B_to_Table': {'precondition': {'A': 'Table', 'B': 'B'}, 'effect': {'B': 'Table'}}
+}
+
+plan = find_plan(initial_state, goal_state, actions)
+print(plan)
+```
+# Example - 2
+```
+initial_state = {'A': 'Table', 'B': 'Table', 'C': 'Table'}
+goal_state = {'A': 'B', 'B': 'C', 'C': 'Table'}
+
+actions = {
+    'move_A_to_B': {'precondition': {'A': 'Table', 'B': 'Table'}, 'effect': {'A': 'B'}},
+    'move_B_to_C': {'precondition': {'A': 'B', 'B': 'Table', 'C': 'Table'}, 'effect': {'B': 'C'}},
+    'move_C_to_Table': {'precondition': {'A': 'B', 'B': 'C', 'C': 'C'}, 'effect': {'C': 'Table'}}
+}
+
+plan = find_plan(initial_state, goal_state, actions)
+print(plan)
+```
+
+```
 def is_goal_state(current_state, goal_state):
     return current_state == goal_state
 
@@ -68,6 +97,8 @@ actions = {
 plan = find_plan(initial_state, goal_state, actions)
 print(plan)
 '''
+
+```
 
 
 
